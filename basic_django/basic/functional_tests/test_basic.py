a6c9a95f9d541ff7b996dc52231fae40ba8de6a4
glickdv <dvgbiogeek@gmail.com>
2015-02-23 12:37:45 -0800

    can now run ft against server

---
+++ basic_django/basic/functional_tests/test_basic.py
@@ -0,0 +1,33 @@
+import sys
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+# import unittest
+
+
+class BasicTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_basic_site(self):
+        self.browser.get(self.server_url)
+        body_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Hello World!', body_text)
